1ecb69f4bc19ef95598241ae72c449548d58ddb5
ayoussfi605-crypto <ayoussfi605@gmail.com>
2026-08-06 19:00:14 +0100

    add renderProfile  func for fetch user id

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,19 @@
 
+import { graphqlRequest } from "./api.js";
+
+
+async function renderProfile() {
+
+    const data = await graphqlRequest(`
+
+            {
+            user{
+                id
+                login
+            }
+            }
+        `)
+        console.log(data);
+}
+
+renderProfile();
